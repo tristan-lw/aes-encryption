ede764128fe231f4a285e89023e37ecb1c55f1f6
tristan-lw <l.tristan.wong@gmail.com>
2024-02-26 18:49:50 +0000

    Added ExpandedKeyBlocks feature

--- AES/KeyExpansion.cs
+++ AES/KeyExpansion.cs
@@ -8,10 +8,14 @@ namespace AES
 {
     internal class KeyExpansion
     {
+        private static Block[] expandedKeyBlocks;
         private Functions func;
         private byte[] word;
         private int byteCounter;
         private int i;
+        private static int numberOfBlocks;
+        private static byte[] expandedKeySliced;
+        private static int counter;
         internal KeyExpansion()
         {
             func = new Functions();
@@ -44,6 +48,40 @@ namespace AES
                     Attributes.ExpandedKey[byteCounter] = (byte)(Attributes.ExpandedKey[byteCounter - 16] ^ word[a]);
                     byteCounter++;
                 }
+            }
+            FillBlocks();
+        }
+        private static void FillBlocks()
+        {
+            // Calculate how many blocks are required
+            numberOfBlocks = Attributes.Key.Length / 16;
+            if (Attributes.Key.Length % 16 != 0)
+            {
+                numberOfBlocks += 1;
+            }
+            expandedKeyBlocks = new Block[numberOfBlocks];
+
+            // Fill each block
+            counter = 0;
+            for (int i = 0; i < expandedKeyBlocks.Length; i++)
+            {
+                //plaintextBytesSliced = new byte[plaintextBytes.Length - counter];
+                //plaintextBytesSliced = plaintextBytes[counter..];
+
+                //ArraySegment<byte[]> plaintextBytesSliced = new ArraySegment<byte[]>(attributes.PlaintextBytes, counter, attributes.PlaintextBytes.Length - counter);
+
+                expandedKeySliced = new byte[Attributes.Key.Length - counter];
+                Array.Copy(Attributes.Key, counter, expandedKeySliced, 0, Attributes.Key.Length - counter);
+
+                expandedKeyBlocks[i] = new Block(expandedKeySliced);
+                counter += 16;
+            }
+            counter = 0;
+            Attributes.ExpandedKeyBlocksString = new string[expandedKeyBlocks.Length];
+            foreach (Block block in expandedKeyBlocks)
+            {
+                Attributes.ExpandedKeyBlocksString[counter] = block.WriteBlock(block);
+                counter++;
             }
         }
     }
